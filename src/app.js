e97a6e5f9b5338fdebfe5db56cd8389759cc7c3a
Kaipo Wilmeth <kaipojames12@gmail.com>
2020-09-17 22:04:18 -1000

    Add first functions to app.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,30 @@
+const createBtn = document.querySelector(".create");
+const notes = document.querySelector(".notes");
+
 const app = {
+  init() {
+    this.addNoteToNotes();
+  },
+
   createElement(element, content) {
     const newElement = document.createElement(element);
     newElement.innerHTML = content;
     return newElement;
+  },
+  getInputValue() {
+    const value = notes.value;
+    return value;
+  },
+  addNoteToNotes() {
+    const value = this.getInputValue();
+    const newNote = this.createElement("p", value);
+    notes.appendChild(newNote);
+  },
+  addEventListeners() {
+    if (createBtn) {
+      createBtn.addEventListener("click", () => {
+        app.init();
+      });
+    }
   },
 };
