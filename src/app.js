33be8ecd86428d704bb8b3b72abb98578359462b
KaipoJames <kaiposemail@yahoo.com>
2020-12-06 21:01:34 -1000

    Implement delete note functionality

--- src/app.js
+++ src/app.js
@@ -20,33 +20,32 @@ const app = {
     const value = input.value;
     return value;
   },
+  createIcon(fileName, parent_div) {
+    const img = document.createElement("img");
+    img.classList.add("icon");
+    img.src = "../icons/" + fileName + ".png";
+    parent_div.appendChild(img);
+  },
   addIcons(note) {
-    const pencil_image = document.createElement("img");
-    const plus_image = document.createElement("img");
-    const trash_image = document.createElement("img");
-    pencil_image.src = "../icons/pencil-sign.png";
-    plus_image.src = "../icons/plus-sign.png";
-    trash_image.src = "../icons/trash-can-sign.png";
-    pencil_image.classList.add("icon");
-    plus_image.classList.add("icon");
-    trash_image.classList.add("icon");
-    note.appendChild(pencil_image);
-    note.appendChild(plus_image);
-    note.appendChild(trash_image);
+    const img_div = document.createElement("div");
+    img_div.classList.add("icons");
+    this.createIcon("pencil-sign", img_div);
+    this.createIcon("plus-sign", img_div);
+    this.createIcon("trash-can-sign", img_div);
+    note.appendChild(img_div);
   },
   createNewNote(inputValue) {
     const note = document.createElement("div");
     const noteText = this.createElement("p", inputValue);
     note.appendChild(noteText);
+    this.addIcons(note);
+    this.implementDeleteFunction(note);
     note.classList.add("note");
     return note;
   },
-
   addNoteToNotes() {
     const inputValue = this.getInputValue();
     const note = this.createNewNote(inputValue);
-    this.addIcons(note);
-
     notes.appendChild(note);
 
     //Store Note in data array
@@ -54,6 +53,20 @@ const app = {
     console.log(data);
     localStorage.setItem("names", JSON.stringify(data));
   },
+  implementDeleteFunction(note) {
+    const icon_div = note.children[1];
+    const delete_icon = icon_div.children[2];
+    const data = note.children[0].innerHTML;
+    delete_icon.addEventListener("click", () => {
+      note.parentNode.removeChild(note);
+      var getNotes = JSON.parse(localStorageContent);
+      getNotes.splice(data,1);
+      localStorage.setItem("names", JSON.stringify(getNotes));
+    });
+  },
+
+
+// <-------- EVENT LISTENERS -------->
   addEventListeners() {
     if (createBtn) {
       createBtn.addEventListener("click", () => {
@@ -76,14 +89,6 @@ const app = {
       });
     }
   },
-  extractData(index) {
-    console.log(data);
-    for (var el of data) {
-      console.log("el: " + el);
-      if (el == index) {
-      }
-    }
-  },
 };
 
 let data;
@@ -97,7 +102,6 @@ if (localStorageContent == null) {
   // Convert data back to HTML elements and add them back to the notes element.
   for (let i = 0; i < data.length; i++) {
     const note = app.createNewNote(data[i]);
-    app.addIcons(note);
     notes.appendChild(note);
   }
 }
